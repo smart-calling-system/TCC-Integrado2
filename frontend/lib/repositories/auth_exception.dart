c608cd826b19eb7aec98a2b40314cf2dd5489799
Neil Lopes <contatowasdgamer@gmail.com>
2026-08-13 22:35:07 -0300

    Frontend novo | FE

--- frontend/lib/repositories/auth_exception.dart
+++
@@ -1,8 +0,0 @@
-/// Erro de autenticação lançado pelo [AuthRepository].
-class AuthException implements Exception {
-  final String mensagem;
-  const AuthException(this.mensagem);
-
-  @override
-  String toString() => mensagem;
-}
